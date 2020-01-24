b3f6a7d09b9656d94fdf3c6fd09d07697e37cc90
scomeaux <scomeaux@vinformatix.com>
2020-01-24 09:08:45 -0600

    loading svg, for flipping screen

---
+++ PhotographyOverlayAttempt2/PhotographyOverlayAttempt2/NewFolder/Loadsvg.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace PhotographyOverlayAttempt2.NewFolder
+{
+    public class Loadsvg
+    {
+        public Loadsvg()
+        {
+            var assembly = IntrospectionExtensions.GetTypeInfo(typeof(Loadsvg)).Assembly;
+            Stream stream = assembly.GetManifestResourceStream("WorkingWithFiles.LibTextResource.txt");
+            string text = "";
+            using (var reader = new StreamReader(stream))
+            {
+                text = reader.ReadToEnd();
+            }
+        }
+    }
+}
